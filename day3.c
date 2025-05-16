8bba95e3e9b5a4bb11570980b50a5a909a190105
asbrubb23aid19 <gokilak23aid@vetias.ac.in>
2025-05-16 15:55:49 +0530

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,72 @@
+/*
+#include <stdio.h>
+
+int main() {
+
+printf("%d",5&4);
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+
+printf("%d",5|4);
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+
+printf("%d",15^11);
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int x = INT_MAX;
+    x =x+1;
+    printf("%d",INT_MAX);
+    printf("\n%d",INT_MIN);
+    printf("\n%d",x);
+    return 0;
+}*/
+
+/*
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    unsigned int x = -455;
+    x =x+1;
+
+    printf("%d",INT_MAX);
+    printf("\n%d",INT_MIN);
+    printf("\n%d",x);
+
+    return 0;
+}
+
+*/
+/*
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int x =10;
+    //x++;
+    //++x;
+    printf("\n%d",x++);
+  
+  
+    return 0;
+}*/
